3e1e484e7ec3e6dbc150dda883706a61bd022f95
claucapo <claucapo@gmail.com>
2018-10-20 16:59:09 -0300

    ultimos cambios

--- send_form_email.php
+++ send_form_email.php
@@ -2,12 +2,12 @@
 if(isset($_POST['email'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "claucapo@gmail.com";
+    $email_to = "juanmanuelhernandez46@gmail.com";
     $email_subject = "Consulta Página Web";
 
     function died($error) {
         // your error code can go here
-        echo "Ocurrió un error. ";
+        echo "Ocurrió un error. ".$error;
         die();
     }
 
@@ -16,7 +16,7 @@ if(isset($_POST['email'])) {
     if(!isset($_POST['nombre_completo']) ||
         !isset($_POST['telefono']) ||
         !isset($_POST['email']) ||
-        !isset($_POST['mensaje']))) {
+        !isset($_POST['mensaje'])) {
         died('Falto completar datos.');
     }
 
@@ -65,15 +65,135 @@ if(isset($_POST['email'])) {
     $email_message .= "Mensaje: ".clean_string($comments)."\n";
 
 // create email headers
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
+$headers = 'From: '.$email_from."\r\n".'Reply-To: '.$email_from."\r\n".'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers);
 ?>
 
 <!-- include your own success html here -->
+<!DOCTYPE html>
+<html lang="en">
 
-Gracias por contactarnos, nos comunicaremos lo antes posible.
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Consultorio Odontológico - Hernandez</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="index.html">Odontología Hernandez</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="about.html">Sobre Nosotros</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="services.html">Servicios</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.html">Contacto</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Más Información
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+                <a class="dropdown-item" href="pricing.html">Precios</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <header>
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+          <!-- Slide One - Set the background image for this slide in the line below -->
+          <div class="carousel-item active" style="background-image: url('./images/sonrisa.jpg')">
+            <div class="carousel-caption d-none d-md-block">
+              <h3>Consultorio Odontológico Hernandez</h3>
+              <p>Consultorio Odontológico ubicado en Belgrano</p>
+            </div>
+          </div>
+          <!-- Slide Two - Set the background image for this slide in the line below -->
+          <div class="carousel-item" style="background-image: url('./images/IMG_1335.JPG')">
+            <div class="carousel-caption d-none d-md-block">
+              <h3>Odontología con Mercado Pago</h3>
+              <p>Ofrecemos diferentes medios de pago - Mercado Pago</p>
+            </div>
+          </div>
+          <!-- Slide Three - Set the background image for this slide in the line below -->
+          <div class="carousel-item" style="background-image: url('./images/IMG_1339.JPG')">
+            <div class="carousel-caption d-none d-md-block">
+              <h3 style="color:black;">Tecnología Dental</h3>
+              <p style="color:black;">Equipamientos dentales de primera línea.</p>
+            </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </header>
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <h1 class="my-4">Gracias por contactarnos, nos comunicaremos lo antes posible.</h1>
+
+      <!-- /.row -->
+
+      <hr/>
+
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Odontología Hernandez 2018</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
+
+
 
 <?php
 
